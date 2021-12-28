f39a23a3c3768b6419b84bbd6965951530ff2e83
lynn <1183521882@qq.com>
2021-12-28 20:21:24 +0800

    fet: 预约登记页面接口联调

--- pages/CashReserve/index.js
+++ pages/CashReserve/index.js
@@ -1,19 +1,8 @@
 // pages/CashReserve/index.js
-const citys = {
-  江西省: [
-    "赣州市",
-    "吉安市",
-    "宜春市",
-    "抚州市",
-    "九江市",
-    "萍乡市",
-    "上饶市",
-    "景德镇市",
-    "南昌市",
-    "新余市",
-  ],
-  福建省: ["厦门市"],
-};
+
+import Toast from "@vant/weapp/toast/toast";
+const createRecycleContext = require("miniprogram-recycle-view");
+var ctx;
 // 获取应用实例
 const app = getApp();
 Page({
@@ -21,20 +10,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    selectedCity: "赣州市",
+    viewHeight: 0,
+    citys: {},
+    selectedCity: "",
+    selectedProvCode: "",
+    selectedCityCode: "",
     active: 0,
     searchVal: "",
     showPicker: false,
-    columns: [
-      {
-        values: Object.keys(citys),
-      },
-      {
-        values: citys["江西省"],
-        defaultIndex: 0,
-      },
-    ],
+    columns: [],
     recordType: 1,
+    bankList: [],
   },
   // 银行预约操作类型
   goCashOpera(event) {
@@ -46,8 +32,13 @@ Page({
   },
   // tab切换
   onChange(event) {
-    console.log(this.data.columns);
     console.log("标签切换成功至", event.detail.name);
+    // TODO: tab切换至我的预约页面，请求数据
+  },
+  // 关键字搜索
+  onSearch(event) {
+    console.log(event.detail);
+    // TODO: 关键字输入确认请求数据
   },
   // 展示城市选择
   openPicker() {
@@ -57,8 +48,8 @@ Page({
   },
   // 选择城市
   handlePicker(event) {
-    const { picker, value, index } = event.detail;
-    picker.setColumnValues(1, citys[value[0]]);
+    const { picker, value } = event.detail;
+    picker.setColumnValues(1, this.data.citys[value[0].text]);
   },
   // 选择器取消
   onPickerCancel() {
@@ -71,21 +62,151 @@ Page({
     this.setData({
       showPicker: false,
     });
-    const { picker, value, index } = event.detail;
-    console.log(`当前值：${value}, 当前索引：${index}`);
+    const { value } = event.detail;
     this.setData({
-      selectedCity: value[1],
+      selectedCity: value[1].text,
+      selectedProvCode: value[0].value,
+      selectedCityCode: value[1].value,
+    });
+    // TODO: 选择器确认请求数据
+  },
+  // 取消预约
+  onCancel() {
+    // TODO: 取消预约的点击事件
+    console.log("取消预约");
+  },
+  // 获取城市数据
+  getCityData() {
+    let cityData = {};
+    let columnData = [];
+    let provList = [];
+    let promiseList = [];
+    // 获取省数据
+    app.service.CashReserve.wxDeptProvAndCityQry().then((res) => {
+      if (res.data.provList && res.data.provList.length > 0) {
+        res.data.provList.forEach((prov) => {
+          provList.push({
+            text: prov.provName,
+            value: prov.provCd,
+          });
+          // 获取市数据
+          promiseList.push(
+            new Promise((resolve, reject) => {
+              app.service.CashReserve.wxDeptProvAndCityQry({
+                provCode: prov.provCd,
+              }).then((result) => {
+                if (result.data.cityList) {
+                  // 市数据格式化
+                  const temp = result.data.cityList.map((city) => {
+                    return {
+                      text: city.cityName,
+                      value: city.cityCode,
+                    };
+                  });
+                  resolve({
+                    key: prov.provName,
+                    value: temp,
+                  });
+                }
+              });
+            })
+          );
+        });
+        Promise.all(promiseList).then((rspList) => {
+          rspList.map((res) => {
+            cityData[res.key] = res.value;
+          });
+          columnData = [
+            {
+              values: provList,
+            },
+            {
+              values: cityData[provList[0].text],
+              defaultIndex: 0,
+            },
+          ];
+          this.getBankList(
+            provList[0].value,
+            cityData[provList[0].text][0].value
+          );
+          this.setData({
+            citys: cityData,
+            columns: columnData,
+            selectedCity: cityData[provList[0].text][0].text,
+          });
+        });
+      }
+    });
+  },
+  getBankList(provCode, cityCode, deptName = "") {
+    wx.getLocation({
+      type: "wgs84",
+      success(res) {
+        app.service.CashReserve.wxQueryDeptListByDist({
+          // distance: "米",
+          longitude: res.longitude,
+          latitude: res.latitude,
+          cityCode: cityCode,
+          provCd: provCode,
+          deptName: deptName,
+          tradeFlag: 1,
+        }).then((content) => {
+          app.service.CashReserve.wxLatelyBookDeptQry({
+            longitude: res.longitude,
+            latitude: res.latitude,
+          }).then((result) => {
+            const list = content.data.list;
+            if (result.data.deptId) {
+              list.unshift({
+                recent: true,
+                addr: result.data.deptAddr,
+                deptId: result.data.deptId,
+                deptName: result.data.deptName,
+                distance: result.data.distance,
+                lat: result.data.lat,
+                lon: result.data.lon,
+                moneyChangeScheduleFlag: result.data.moneyChangeScheduleFlag,
+                onlineLargeCashBookFlag: result.data.onlineLargeCashBookFlag,
+              });
+            }
+            ctx.append(content.data.list);
+          });
+        });
+      },
+      fail() {
+        Toast("获取当前位置失败，请重新授权！");
+      },
     });
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function () {
+    this.getCityData();
+    // this.getBankList();
+  },
+
+  itemSizeFunc: function (item, idx) {
+    return {
+      width: ctx.transformRpx(750),
+      height: ctx.transformRpx(240),
+    };
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {},
+  onReady: function () {
+    ctx = createRecycleContext({
+      id: "recycleId",
+      dataKey: "bankList",
+      page: this,
+      itemSize: this.itemSizeFunc,
+    });
+    this.setData({
+      viewHeight: wx.getSystemInfoSync().windowHeight - 98,
+    });
+  },
 
   /**
    * 生命周期函数--监听页面显示
--- service/CashReserve.js
+++ service/CashReserve.js
@@ -1,2 +1,78 @@
 const { get, post, put } = require("../utils/util");
-export default {};
+export default {
+  /**
+   * 网点信息查询
+   * @param {
+   * provCode: 省代码(不传查省，传值查当前省下面的市)
+   * }
+   */
+  wxDeptProvAndCityQry(params) {
+    return post("wxDeptProvAndCityQry.do", params);
+  },
+  /**
+   * 最近预约机构查询
+   * @param {
+   * longitude: 经度
+   * latitude: 纬度
+   * }
+   */
+  wxLatelyBookDeptQry(params) {
+    return post("wxLatelyBookDeptQry.do", params);
+  },
+  /**
+   * 附近网点查询
+   * @param {
+   * distance: 距离单位，单位：米
+   * longitude: 经度
+   * latitude: 纬度
+   * cityCode: 市代码
+   * provCd: 省代码
+   * deptName: 机构名称
+   * tradeFlag: 1.现金预约；2.网点查询
+   * }
+   */
+  wxQueryDeptListByDist(params) {
+    return post("wxQueryDeptListByDist.do", params);
+  },
+  /**
+   * 预约日期查询
+   */
+  wxLargeCashBookDateQry() {
+    return post("wxLargeCashBookDateQry.do");
+  },
+  /**
+   * 预约操作
+   * @param {
+   * bsTy:
+   * wddt:
+   * wdtm:
+   * amcr:
+   * wdAm:
+   * list:
+   * }
+   */
+  wxLargeCashBook(params) {
+    return post("wxLargeCashBook.do", params);
+  },
+  /**
+   * 预约记录查询
+   */
+  wxLargeCashBookQry() {
+    return post("wxLargeCashBookQry.do");
+  },
+  /**
+   * 取消预约操作
+   * @param {
+   * operationType:
+   * businessType:
+   * widtdrawDate:
+   * cardId:
+   * widtdrawTime:
+   * deptId:
+   * FromUserName:
+   * }
+   */
+  wxLargeCashBookCancel(params) {
+    return post("wxLargeCashBookCancel.do", params);
+  },
+};
